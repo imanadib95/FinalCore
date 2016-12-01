dbf9b39b84e132070b9e6513ed1bfa8beb1f233b
Iman Adibnazari <imanadib95@gmail.com>
2016-11-30 17:21:19 -0700

    Small changes

--- Core_MemCont_VGA.v
+++ Core_MemCont_VGA.v
@@ -44,37 +44,31 @@ JA, JD, JC);
 	// Wires for Knobs
 	wire [15:0] xPixelPos, yPixelPos, colorPos;//**pixel output 
 	wire [1:0] xDir, yDir, colorDir; 
-
-
-	
-	
-	wire [15:0] UDcount,LRcount;
+	// Wires for button control of cursor
+		// wire [15:0] UDcount,LRcount;
 	// Instantiate modules
 	CoreTop Core(clk,CoreDataOut, CoreAddress,CoreWriteData,CoreWE
 	//,R1Out,R2Out
 	);
 	
 	
-	// Kara, if you want to wire in the nobs, then go down to where xPixelPose, yPixelPos and colorPos
-	// are commented out in the module below and switch them in for the inputs that are already in those
-	//	lines. Just make sure theyre outputting properly and you should be good.
-	MemCont Controller(clk, 
+	
+	MemCont MemoryController(clk, 
 	CoreWE, 
 	CoreWriteData,	
 	CoreAddress, 
 	VGAAddress, 
 	{15'd0,rstBtn},
-	(xPixelPos+2'd2), //LRcount,
-	(16'd120-yPixelPos), //UDcount, //
-	colorPos, //{8'b0,switches}, //
+	(xPixelPos+2'd2), 
+	(16'd120-yPixelPos),  
+	colorPos, 
 	CoreDataOut, 
 	VGADataOut,
 	accBeenRead);
 	
 	VGA_Controller VGA(clk, VGADataOut[7:0],VGAAddress,Hsync,Vsync,ColorOut);
 	
-/*	// Instantiations for controlling cursor with onboard buttons
-	
+/*	// Controlling cursor with onboard buttons
 	
 	debounce upbtn(1'b0, clk, up, updb);
 	
@@ -93,7 +87,7 @@ JA, JD, JC);
 	
 	BinaryTo4DigHex7SegDisplay	 DisplayConverter(en_bus, ContBinOUT,CA,CB,CC,CD,CE,CF,CG,DP,AN0, AN1, AN2, AN3);
 	
-	// Encoder business
+	// Optical Encoder Modules
 	PmodEnc #(159) xEnc (clk, JA, xPixelPos[7:0]); 
 	PmodEnc #(119) yEnc (clk, JC, yPixelPos[7:0]); 
 	PmodEnc #(255) color(clk, JD, colorPos[7:0]);
